dbb557132e9c429e0eb41df0b989ddd1115a94a6
undirectlookable <undirectlookable@users.noreply.github.com>
2017-10-20 10:58:18 +0800

    complete codelab, add public key input field

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,9 +21,10 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+var applicationServerPublicKey = '';
 
 const pushButton = document.querySelector('.js-push-btn');
+const publicKeyInput = document.querySelector('.js-publickey-input');
 
 let isSubscribed = false;
 let swRegistration = null;
@@ -41,4 +42,139 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+    initializeUI();
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+function initializeUI() {
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+  });
+
+  const previousPublicKey = localStorage.getItem('ServerPublicKey') || '';
+  publicKeyInput.addEventListener('keyup', updateBtn);
+  publicKeyInput.value = previousPublicKey;
+  publicKeyInput.focus()
+  updateBtn();
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    updateSubscriptionOnServer(subscription);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+    pushButton.disabled = false;
+  } else if (getPublicKey() === null) {
+    pushButton.textContent = 'Enter Public Key First';
+    pushButton.disabled = true;
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+    pushButton.disabled = false;
+  }
+}
+
+function subscribeUser() {
+  applicationServerPublicKey = getPublicKey();
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    localStorage.setItem('ServerPublicKey', applicationServerPublicKey);
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+function getPublicKey() {
+  const val = publicKeyInput.value
+  return (val && val.length === 87) ? val : null
 }
